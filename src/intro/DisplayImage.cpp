de6850c62ee2b7f69e4c00226dd659f17919da9f
Zardosht Hodaie <zardosht@hodaie.com>
2014-08-04 00:23:25 +0200

    commented out

--- src/intro/DisplayImage.cpp
+++ src/intro/DisplayImage.cpp
@@ -5,32 +5,32 @@
  *      Author: zardosht
  */
 
-//#include <opencv2/core/core.hpp>
-//#include <opencv2/highgui/highgui.hpp>
-//#include <iostream>
-//
-//using namespace cv;
-//using namespace std;
-//
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
 int main(int argc, char** argv)
 {
-//	if(argc != 2){
-//		cout << "Usage: DisplayImage <image-to-display>"  << endl;
-//		return -1;
-//	}
-//
-//	Mat image;
-//	image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-//
-//	if(!image.data){
-//		cout << "Could not open or find the image." << endl;
-//		return -1;
-//	}
-//
-//	namedWindow("Display Window", WINDOW_AUTOSIZE);
-//	imshow("Display Window", image);
-//
-//	waitKey(0);
-//	return 0;
+	if(argc != 2){
+		cout << "Usage: DisplayImage <image-to-display>"  << endl;
+		return -1;
+	}
+
+	Mat image;
+	image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+
+	if(!image.data){
+		cout << "Could not open or find the image." << endl;
+		return -1;
+	}
+
+	namedWindow("Display Window", WINDOW_AUTOSIZE);
+	imshow("Display Window", image);
+
+	waitKey(0);
+	return 0;
 
 }
